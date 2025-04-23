391cc2cf683eb91fecfccffd628766a0f0c7d5dc
Sharib Kabani <ksharib05@gmail.com>
2025-04-23 18:20:56 -0400

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -13,22 +13,6 @@ import (
 	"github.com/sharibkabani/desygn-backend/handlers"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func main() {
 	// Load .env variables (e.g., database URL, Gemini API key)
 	err := godotenv.Load()
